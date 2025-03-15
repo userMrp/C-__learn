bddf363e883d2643550cc5202210b90f16b7e870
userMrp <113045114+userMrp@users.noreply.github.com>
2025-03-15 13:57:41 +0800

    Add files via upload

---
+++ day1_1.1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "多main解决方案" << endl;//shift+alt+键盘上下，控制当前行上下移动
+    cout << "多main解决方案" << endl;//ctrl+D 快速复制
+    cout << "多main解决方案" << endl;//ctrl+键盘左右键，光标以单词为单位左右移动
+    cout << "多main解决方案" << endl;
+    return 0;
+}
